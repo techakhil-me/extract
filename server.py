b5ff298ba465c4d0456afe976ae5086b1ec4d72a
Akhil Chathoth Padmanabhan <43501564+techakhil-me@users.noreply.github.com>
2024-03-16 15:35:32 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.applications.vgg16 import preprocess_input
+import os
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+# from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+model = load_model('Xtract2.h5')
+target_img = os.path.join(os.getcwd() , 'static/images')
+@app.route('/')
+def index_view():
+    return render_template('index.html')
+#Allow files with extension png, jpg and jpeg
+ALLOWED_EXT = set(['jpg' , 'jpeg' , 'png'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+           
+# Function to load and prepare the image in right shape
+def read_image(filename):
+    img = load_img(filename, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
+
+@app.route('/predict',methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename): #Checking file format
+            filename = file.filename
+            file_path = os.path.join('static/images', filename)
+            file.save(file_path)
+            img = read_image(file_path)
+             #prepressing method
+            test_img = Image.open(file_path)
+            test_img = test_img.resize((224,224))
+            test_img = np.array(test_img) /255.0
+            test_img = np.expand_dims(test_img, axis=0)
+            class_prediction=model.predict(test_img) 
+            classes_x=np.argmax(class_prediction)
+            print(classes_x)
+            if classes_x == 0:
+              cancer = "Detected Lung adenocarcinoma"
+            else:
+              cancer = "Not detected Lung adenocarcinoma"
+           
+            if classes_x == 0:
+              fruit = "ACA"
+            elif classes_x == 1:
+              fruit = "N"
+            else:
+              fruit = "SCC"
+            #'fruit' , 'prob' . 'user_image' these names we have seen in predict.html.
+            return render_template('predict.html', user_image = file_path, cancer = cancer, fruit = fruit)
+        else:
+            return "Unable to read the file. Please check file extension"
+        
+if __name__ == '__main__':
+    app.run(debug=True,use_reloader=False, port=8000)
